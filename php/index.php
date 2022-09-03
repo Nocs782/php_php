d0d6820dd9d9a7b371a6c98ed053ef22c34c614f
Nocs782 <emailforuniversity99@gmail.com>
2022-09-03 17:16:12 +0600

    first commit

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Hello</h1>
+    <?php
+        $MY_VAR = 5;
+        echo gettype($MY_VAR);
+        if(isset($MY_VAR)){
+            echo " my dick is big my dick is very big";
+        }
+    ?>
+</body>
+</html>
