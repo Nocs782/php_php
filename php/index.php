6c1cfd5344ad3f8d6e5d8dc6c2054f9c8cb7b745
Nocs782 <emailforuniversity99@gmail.com>
2022-11-30 17:30:38 +0600

    thelogin

--- php/index.php
+++ php/index.php
@@ -1,20 +1,23 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1> Hello</h1>
-    <?php
-        $MY_VAR = 5;
-        echo gettype($MY_VAR);
-        if(isset($MY_VAR)){
-            echo " my dick is big my dick is very big";
-        }
-    ?>
-</body>
-</html>
+<?php
+
+$servername ="localhost";
+$username = "root";
+
+$password = "";
+
+$conn = new mysqli($servername, $username, $password);
+
+if($conn->connect_error){
+    die("connection is failed" . $conn->connect_error);
+}
+
+$sql  = "Create database testDB";
+
+if($conn->query($sql) == TRUE){
+    echo "database creation succeeded";
+}else {
+
+    echo "Error" .$conn->error;
+}
+
+?>
